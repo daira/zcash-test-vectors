ccfc1ccefec43d26a7df9dbc65869f4f6b874861
Daira Hopwood <daira@jacaranda.org>
2018-06-04 15:30:05 +0100

    Rename CRS to URS, matching version beta-21 of Sapling spec.
    Signed-off-by: Daira Hopwood <daira@jacaranda.org>

--- sapling_generators.py
+++ sapling_generators.py
@@ -9,7 +9,7 @@ from sapling_utils import chunk
 # This is chosen to be some random string that we couldn't have
 # anticipated when we designed the algorithm, for rigidity purposes.
 # We deliberately use an ASCII hex string of 32 bytes here.
-CRS = b'096b36a5804bfacef1691e173c366a47ff5ba84a44f26ddd7e8d9f79d5b42df0'
+URS = b'096b36a5804bfacef1691e173c366a47ff5ba84a44f26ddd7e8d9f79d5b42df0'
 
 
 #
@@ -18,7 +18,7 @@ CRS = b'096b36a5804bfacef1691e173c366a47ff5ba84a44f26ddd7e8d9f79d5b42df0'
 
 def group_hash(D, M):
     digest = blake2s(person=D)
-    digest.update(CRS)
+    digest.update(URS)
     digest.update(M)
     p = Point.from_bytes(digest.digest())
     if not p:
